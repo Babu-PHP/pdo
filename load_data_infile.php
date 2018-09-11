3ebadd977a4f45c7f428b4717bae5bb157bbdefa
Babu-PHP <bheema.babi@gmail.com>
2018-09-11 16:00:10 +0530

    Added Code

--- load_data_infile.php
+++ load_data_infile.php
@@ -16,6 +16,7 @@ if ($db) {
 	echo "Error name : " . $dberror[2] . "<br>";
 }
 ?>
+<!-------------  upload a CSV in PDO ---------------->
 <?php
 /* $cons= mysqli_connect("localhost", "root","root","promocode") or die(mysql_error());
 
@@ -55,4 +56,47 @@ if ($db) {
 						  if(!$affectedRows){
 						  	$sucess = "Error occurred";
 						  }
+?>
+
+<!------------- If Comma(,) character in any string in CSV file, Use the PHP Script----------------->
+<?php
+$databasehost = "localhost"; 
+$databasename = "import"; 
+$databasetable = "import"; 
+$databaseusername="username"; 
+$databasepassword = "password"; 
+$fieldseparator = ","; 
+$lineseparator = "\n";
+
+$enclosedby = "\"";
+
+$csvfile = "test.csv";
+
+if(!file_exists($csvfile)) {
+    die("File not found. Make sure you specified the correct path.");
+}
+
+try {
+    $pdo = new PDO("mysql:host=$databasehost;dbname=$databasename", 
+        $databaseusername, $databasepassword,
+        array(
+            PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        )
+    );
+} catch (PDOException $e) {
+    die("database connection failed: ".$e->getMessage());
+}
+
+$pdo->exec("TRUNCATE TABLE `$databasetable`");
+
+    $affectedRows = $pdo->exec("
+    LOAD DATA LOCAL INFILE ".$pdo->quote($csvfile)." REPLACE INTO TABLE `$databasetable`
+    FIELDS TERMINATED BY ".$pdo->quote($fieldseparator)." 
+    ENCLOSED BY ".$pdo->quote($enclosed)." 
+    LINES TERMINATED BY ".$pdo->quote($lineseparator)."
+    IGNORE 1 LINES");
+
+echo "Loaded a total of $affectedRows records from this csv file.\n";
+
 ?>
